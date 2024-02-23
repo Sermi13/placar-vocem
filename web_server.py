6f630ce6baff3f98fa113775efb83cd859d5778a
Sermi_fox <69096299+Sermi13@users.noreply.github.com>
2024-02-23 17:08:50 -0300

    Update web_server.py

--- web_server.py
+++ web_server.py
@@ -19,5 +19,5 @@ def get_soup():
     return rendered_html
 
 
-if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0",port=8080)
+# if __name__ == '__main__':
+#     app.run(debug=False, host="0.0.0.0",port=8080)
