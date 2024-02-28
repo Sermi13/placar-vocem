5f50c3a619a62a3db60b6dc8cd730f5b51a96e59
Sermi <ricobordinx@gmail.com>
2024-02-28 14:27:23 -0300

    Accepts link param

--- web_server.py
+++ web_server.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import re
 import requests
+from urllib.parse import unquote
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def get_soup():
-    url = 'https://www.futebolinterior.com.br/campeonato/paulista-a4-2024/'
+    url = unquote(request.args.get("link"))
+
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
     site = requests.get(url, headers=headers)
@@ -17,4 +21,4 @@ def get_soup():
 
 
 # if __name__ == '__main__':
-#     app.run(debug=False, host="0.0.0.0",port=8080)
+    # app.run(debug=True, port=8080)
